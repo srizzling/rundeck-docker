002e92ed9950f6fc45b7f42dda65a298fc3882cf
Sriram Venkatesh <venksriram@gmail.com>
2016-03-29 14:25:53 +1300

    improve makefile

--- plugins/docker-nodes-images/src/contents/nodes.sh
+++ plugins/docker-nodes-images/src/contents/nodes.sh
@@ -15,15 +15,12 @@ docker images | cut -f1 -d" " | grep -v "REPOSITORY" | grep -v "centos" | while 
 do
         Created=$(docker inspect -f '{{.Created}}' $image)
         imageId=$(docker inspect -f '{{.Id}}' $image)
-
         hostname=$(docker inspect -f '{{.Config.Hostname}}' $image)
         username=$(docker inspect -f '{{.Config.User}}' $image)
         exposed_ports=$(docker inspect -f '{{.Config.ExposedPorts}}' $image)
         image=$image
         WorkingDir=$(docker inspect -f '{{.Config.WorkingDir}}' $image)
         name=$image
-
-
 cat <<EOF
 <node  name='$image' description='$name' imageId='$imageId' username='$username' tags='image,$image' hostname='$hostname' >
 <attribute name="ExposedPorts" value='$exposed_ports'/>
